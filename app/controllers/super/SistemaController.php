bf0e1ada009595ac573688536e5c48417134b677
Kerberitos <kerberitos@hotmail.com>
2016-03-18 18:30:36 -0500

    dos

---
+++ app/controllers/super/SistemaController.php
@@ -0,0 +1,62 @@
+<?php  namespace super;
+
+use Anuncia\Repositorios\AnuncioRepo;
+use Anuncia\Repositorios\NotificacionRepo;
+
+/**
+ * ----------------------------------------------------
+ * Clase que : 
+ * 		- Muestra el Panel Sistema de la aplicación
+ * ----------------------------------------------------
+ * Rutas:
+ * 		- miradita/app/routes/super.php
+ *		
+ * ----------------------------------------------------
+ * autor: Edison Alexander Rojas León
+ * email: 
+ * fecha: 00/00/0000
+ *
+ */
+
+
+class SistemaController extends \BaseController
+{
+	protected $anuncioRepo;
+	protected $notificacionRepo;
+	
+	public function __construct(AnuncioRepo $anuncioRepo,
+								NotificacionRepo $notificacionRepo)
+	{
+		$this->anuncioRepo = $anuncioRepo;
+		$this->notificacionRepo = $notificacionRepo;
+	}
+
+	/* Muestra el Panel de Sistema */
+	public function panelSistema()
+	{
+		
+		$numAnunciosExpirados = $this->getNumeroAnunciosExpirados();
+		$numNotificacionesExpiradas = $this->getNumeroNotificacionesExpiradas();
+
+		return \View::make('modulos.super.panelsystem', 
+							compact('numAnunciosExpirados',
+									'numNotificacionesExpiradas')
+
+				);
+
+
+	}
+
+	/* Obtiene el número de anuncios expirados */
+	public function getNumeroAnunciosExpirados()
+	{
+		return $this->anuncioRepo->enumerarAnunciosExpirados();		
+	}
+	
+	/* Obtiene el número de notificaciones expiradas */
+	public function getNumeroNotificacionesExpiradas()
+	{
+		return $this->notificacionRepo->enumerarNotificacionesExpiradas();		
+	}
+
+}
